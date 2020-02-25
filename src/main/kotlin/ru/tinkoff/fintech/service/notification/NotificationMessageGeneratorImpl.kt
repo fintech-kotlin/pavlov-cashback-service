89c7cf31224fe5c77dc39a2080ea629d22e8a443
AleksandrPavlov <dumadumae@mail.ru>
2020-02-25 08:26:24 +0300

    Добавляем реализацию с исправлениями.

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/NotificationMessageGeneratorImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/NotificationMessageGeneratorImpl.kt
@@ -3,10 +3,16 @@ package ru.tinkoff.fintech.service.notification
 import ru.tinkoff.fintech.model.NotificationMessageInfo
 
 class NotificationMessageGeneratorImpl(
-    private val cardNumberMasker: CardNumberMasker
-) : NotificationMessageGenerator {
+
+    private val cardNumberMasker: CardNumberMasker) : NotificationMessageGenerator {
 
     override fun generateMessage(notificationMessageInfo: NotificationMessageInfo): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return with(notificationMessageInfo){
+            "Уважаемый, ${name}!\n" +
+                    "Спешим Вам сообщить, что на карту ${cardNumberMasker.mask(notificationMessageInfo.cardNumber)}\n" +
+                    "начислен cashback в размере ${notificationMessageInfo.cashback}\n" +
+                    "за категорию ${notificationMessageInfo.category}.\n" +
+                    "Спасибо за покупку ${notificationMessageInfo.transactionDate}"
+        }
     }
 }
