5eb2ba44505fee5b57b42edaa392e7d883211b9f
AleksandrPavlov <dumadumae@mail.ru>
2020-03-04 09:02:48 +0300

    Исправляем замечания, оптимизируем код.

--- src/main/kotlin/ru/tinkoff/fintech/service/cashback/CashbackCalculatorImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/cashback/CashbackCalculatorImpl.kt
@@ -21,99 +21,89 @@ class CashbackCalculatorImpl : CashbackCalculator {
             if (cashbackTotalValue >= MAX_CASH_BACK) {
                 return 0.0
             }
-            var cashback = 0.0
-
-            cashback += programmBlack(this)
-            cashback += additionalCashBack(this)
-            cashback += programmAll(this)
-            cashback += programmBeer(this)
-
-            if (cashback + cashbackTotalValue > MAX_CASH_BACK) {
-                return MAX_CASH_BACK - cashbackTotalValue
+            var cashback: Double
+            cashback = when (loyaltyProgramName) {
+                "BLACK" -> programmBlack(this)
+                "ALL" -> programmAll(this)
+                "BEER" -> programmBeer(this)
+                else -> return 0.0
             }
-            return cashback
+            cashback += additionalCashback(this)
+            return if (cashback + cashbackTotalValue > MAX_CASH_BACK) {
+                MAX_CASH_BACK - cashbackTotalValue
+            } else {
+                cashback
+            }
         }
     }
 
     private fun programmBlack(transactionInfo: TransactionInfo): Double {
-        with(transactionInfo) {
-            if (loyaltyProgramName.equals(LOYALTY_PROGRAM_BLACK)) {
-                return transactionSum / 100 * 1
-            }
-            return 0.0
-        }
+        return transactionInfo.transactionSum / 100 * 1
     }
 
-    private fun programmAll(transactionInfo: TransactionInfo): Double {
-        with(transactionInfo) {
-            if (mccCode?.equals(MCC_SOFTWARE) ?: false
-                && loyaltyProgramName.equals(LOYALTY_PROGRAM_ALL)
-                && chekPalindrom(transactionSum)
-            ) {
-                val a = firstName.length
-                val b = lastName.length
-                val lcm = lcm(a, b)
-                return (lcm / 1000.0 / 100 * (transactionSum)).toBigDecimal()
-                    .setScale(1, RoundingMode.HALF_EVEN).toDouble()
-            }
-        }
-        return 0.0
-    }
-
-
-
-    private fun programmBeer(transactionInfo: TransactionInfo): Double {
-        with(transactionInfo) {
-            if (loyaltyProgramName.equals(LOYALTY_PROGRAM_BEER) && mccCode!!.equals(
-                    MCC_BEER
-                )
-            ) {
-
-                if (firstName.toLowerCase().equals("олег")) {
-                    if (lastName.toLowerCase().equals("олегов")) {
-                        return transactionSum / 100 * 10
-                    } else {
-                        return transactionSum / 100 * 7
-                    }
-                }
-
-                if (LocalDateTime.now().month.getDisplayName(
-                        TextStyle.SHORT,
-                        Locale("ru")
-                    ).toLowerCase().first().equals(
-                        firstName.toLowerCase().first()
-                    )
-                ) {
-                    return transactionSum / 100 * 5
-                }
-
-                if (LocalDateTime.now().plusMonths(1).month.getDisplayName(
-                        TextStyle.SHORT,
-                        Locale("ru")
-                    ).toLowerCase().first().equals(firstName.toLowerCase().first())
-                    || LocalDateTime.now().minusMonths(1).month.getDisplayName(
-                        TextStyle.SHORT,
-                        Locale("ru")
-                    ).toLowerCase().first().equals(firstName.toLowerCase().first())
-                ) {
-                    return transactionSum / 100 * 3
-
-                }
-
-                return transactionSum / 100 * 2
-            }
-        }
-        return 0.0
-    }
-
-    private fun additionalCashBack(transactionInfo: TransactionInfo): Double {
+    private fun additionalCashback(transactionInfo: TransactionInfo): Double {
         if (transactionInfo.transactionSum.rem(666).equals(0.0)) {
             return 6.66
         }
         return 0.0
     }
 
-    private fun chekPalindrom(double: Double): Boolean {
+
+    private fun programmAll(transactionInfo: TransactionInfo): Double {
+        with(transactionInfo) {
+            return if (mccCode?.equals(MCC_SOFTWARE) == true
+                && isPalindrom(transactionSum)
+            ) {
+                val lcm = lcm(firstName.length, lastName.length)
+                (lcm / 1000.0 / 100 * (transactionSum)).toBigDecimal()
+                    .setScale(1, RoundingMode.HALF_EVEN).toDouble()
+            } else {
+                0.0
+            }
+        }
+    }
+
+    private fun programmBeer(transactionInfo: TransactionInfo): Double {
+        with(transactionInfo) {
+            if (loyaltyProgramName == LOYALTY_PROGRAM_BEER && mccCode!! == MCC_BEER) {
+
+                if (firstName.toLowerCase() == "олег") {
+                    return if (lastName.toLowerCase() == "олегов") {
+                        transactionSum / 100 * 10
+                    } else {
+                        transactionSum / 100 * 7
+                    }
+                }
+
+                return if (LocalDateTime.now().month.getDisplayName(
+                        TextStyle.SHORT,
+                        Locale("ru")
+                    ).toLowerCase().first() == firstName.toLowerCase().first()
+                ) {
+                    transactionSum / 100 * 5
+                } else if (compareFirstSignOnRoundMonthAndName(firstName)) {
+                    transactionSum / 100 * 3
+                } else transactionSum / 100 * 2
+
+            } else {
+                return 0.0
+            }
+        }
+    }
+
+    private fun compareFirstSignOnRoundMonthAndName(firstName: String): Boolean {
+        return (LocalDateTime.now().plusMonths(1).month.getDisplayName(
+            TextStyle.SHORT,
+            Locale("ru")
+        ).toLowerCase().first() == firstName.toLowerCase().first()
+                || LocalDateTime.now().minusMonths(1).month.getDisplayName(
+            TextStyle.SHORT,
+            Locale("ru")
+        ).toLowerCase().first() == firstName.toLowerCase().first()
+                )
+    }
+
+    private fun isPalindrom(double: Double): Boolean {
         val st = double.toString().filter { char -> !char.equals('.') }
         var t = 0
         val l = st.length - 1
@@ -126,7 +116,7 @@ class CashbackCalculatorImpl : CashbackCalculator {
     }
 
     private fun gcd(a: Int, b: Int): Int {
-        return if (b === 0) a else gcd(b, a % b)
+        return if (b == 0) a else gcd(b, a % b)
     }
 
     private fun lcm(a: Int, b: Int): Int {
